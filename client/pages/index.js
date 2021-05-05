bd79b4c3665f63ee755bbaf60734dabf72e92f35
“lucifer2355” <dgajjar999@gmail.com>
2021-05-05 22:29:52 +0530

    redirect to auth page when token is null

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import Head from "next/head";
-import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    token === null ? router.push("/auth") : null;
+  }, []);
+
   return (
     <div>
       <Head>
